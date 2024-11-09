b4b9f880ae118b5b5f5a9e7c51709395445a2308
frank2 <frank2@dc949.org>
2024-11-08 17:49:53 -0800

    try this

--- main.c
+++ main.c
@@ -17,8 +17,18 @@ uint32_t fnv321a(const char *str) {
    return hash;
 }
 
+uint8_t *get_import_by_hash(uint8_t *module, uint32_t hash) {
+   PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)module;
+   PIMAGE_NT_HEADERS64 nt_headers = (PIMAGE_NT_HEADERS64)&module[dos_header->e_lfanew];
+   PIMAGE_DATA_DIRECTORY export_datadir = &nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+   PIMAGE_EXPORT_DIRECTORY export_dir = (PIMAGE_EXPORT_DIRECTORY)&module[export_datadir->VirtualAddress];
+
+   return NULL;
+}
+
 int main(int argc, char *argv[]) {
-   return 0;
+   uint32_t (*ptr)(void) = target_function;
+   uint32_t value = ptr();
 }
 #pragma code_seg(pop, r1)
 
